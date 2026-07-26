c9201d42beea0bb23ced951fdcec082459870027
Joey Fernandez <joey.fernandez3@gmail.com>
2026-07-26 00:53:32 -0400

    Pin engine v0.6.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ const appRoutes = [
 ];
 
 export default defineNuxtConfig({
-  extends: [process.env.ENGINE_PATH || ['github:joeycf/replay-engine#v0.6.0', { install: true }]],
+  extends: [process.env.ENGINE_PATH || ['github:joeycf/replay-engine#v0.6.1', { install: true }]],
 
   compatibilityDate: '2025-07-01',
 
